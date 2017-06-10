d23f7ab0896baf4281ebd4d055d8e3e1563f823e
martin gonzalvo <gonzalvomartin@gmail.com>
2017-06-09 23:59:43 -0300

    subida proyecto
    se sube el proyecto de cines lavalle

---
+++ Peliculas/Controllers/AdministracionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Peliculas.Controllers
+{
+    public class AdministracionController : Controller
+    {
+        // GET: Administracion
+        public ActionResult Administracion()
+        {
+            return View("Login");
+        }
+    }
+}
---
+++ Peliculas/Models/Registro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Peliculas.Models
+{
+    public class Registro
+    {
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string email { get; set; }
+        public string pass { get; set; }
+    }
+}
